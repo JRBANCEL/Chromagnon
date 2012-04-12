c21d330010af4715c2315337ab30286fbb9f9329
Jean-Rémy Bancel <jrbleboss@gmail.com>
2012-04-12 22:55:59 +0200

    Starting using the modules.

--- chromagnon/chromagnon.py
+++ chromagnon/chromagnon.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-
 import sqlite3
 import sys
 
 from classicalOutput import classicalOutput
 from columnOutput import columnOutput
 from csvOutput import csvOutput
+import historyInput
 
-#XXX
-db = sqlite3.connect("../data/History")
 
 # Seems to be weird but if sys module is imported once, the setdefaultencoding
 # function is not available. Whatever, set utf-8 for the whole project.
@@ -18,7 +16,5 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
-queryResult = db.execute("select * from urls")
-#classicalOutput(queryResult, ';')
+classicalOutput(historyInput.parse(), ';')
 #csvOutput(queryResult)
-columnOutput(queryResult)
