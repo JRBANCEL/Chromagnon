7e19d74ddbd2f852ae856146577bd4fff3be2284
Jean-Rémy Bancel <jrbleboss@gmail.com>
2012-04-16 18:06:45 +0200

    Creating Cache Parsing File.

---
+++ chromagnon/cacheParse.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Parse the Chrome Cache File
+See http://www.chromium.org/developers/design-documents/network-stack/disk-cache
+for design details
+"""
+
+import struct
+
+def parse(filename="../data/Cache/index"):
+    """
+    Reads the whole cache and store the collected data in a table
+    """
+    pass
