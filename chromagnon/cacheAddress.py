25ab3355338e12a188b2dea47516ad50cf02cd0f
Jean-Rémy Bancel <jrbleboss@gmail.com>
2012-04-19 00:28:13 +0200

    Adding some attributes : 	- path (path of the cache folder) 	- entrySize Raising an exception in case of uninitialized address.

--- chromagnon/cacheAddress.py
+++ chromagnon/cacheAddress.py
@@ -16,16 +16,20 @@ class CacheAddress():
     BLOCK_1024 = 3
     BLOCK_4096 = 4
 
-    typeArray = ["Separate file",
-                 "Ranking block file",
-                 "256 bytes block file",
-                 "1k bytes block file",
-                 "4k bytes block file"]
+    typeArray = [("Separate file", 0),
+                 ("Ranking block file", 36),
+                 ("256 bytes block file", 256),
+                 ("1k bytes block file", 1024),
+                 ("4k bytes block file", 4096)]
 
-    def __init__(self, uint_32):
+    def __init__(self, uint_32, path):
         """
         Parse the 32 bits of the uint_32
         """
+        if uint_32 == 0:
+            raise Exception("Uninitialized Address")
+
+        self.path = path
         self.binary = bin(uint_32)
         self.blockType = int(self.binary[3:6], 2)
 
@@ -33,9 +37,10 @@ class CacheAddress():
         if self.blockType == CacheAddress.SEPARATE_FILE:
             self.fileSelector = "f_%05X"%int(self.binary[6:], 2)
         else:
+            self.entrySize = CacheAddress.typeArray[self.blockType][1]
             self.contiguousBlock = int(self.binary[8:10], 2)
             self.fileSelector = "data_" + str(int(self.binary[10:18], 2))
             self.blockNumber = int(self.binary[18:], 2)
 
     def __str__(self):
-        return hex(self.binary), CacheAddress.typeArray[self.blockType]
+        return hex(self.binary), CacheAddress.typeArray[self.blockType][0]
