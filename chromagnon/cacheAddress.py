cc20516f63447242924c9c94a3db6f1d1c1df98d
Jean-Rémy Bancel <jrbleboss@gmail.com>
2012-04-20 22:44:14 +0200

    Adding custom Exception. Checking if the address is correct (MSB).

--- chromagnon/cacheAddress.py
+++ chromagnon/cacheAddress.py
@@ -6,6 +6,12 @@ Chrome Cache Address
 See /net/disk_cache/addr.h for design details
 """
 
+class CacheAddressError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
 class CacheAddress():
     """
     Object representing a Chrome Cache Address
@@ -27,15 +33,22 @@ class CacheAddress():
         Parse the 32 bits of the uint_32
         """
         if uint_32 == 0:
-            raise Exception("Uninitialized Address")
+            raise CacheAddressError("Null Address")
 
+        #XXX Is self.binary useful ??
+        self.addr = uint_32
         self.path = path
+
+        # Checking that the MSB is set
         self.binary = bin(uint_32)
+        if len(self.binary) != 34:
+            raise CacheAddressError("Uninitialized Address")
+
         self.blockType = int(self.binary[3:6], 2)
 
         # If it is an address of a separate file
         if self.blockType == CacheAddress.SEPARATE_FILE:
-            self.fileSelector = "f_%05X"%int(self.binary[6:], 2)
+            self.fileSelector = "f_%06x"%int(self.binary[6:], 2)
         else:
             self.entrySize = CacheAddress.typeArray[self.blockType][1]
             self.contiguousBlock = int(self.binary[8:10], 2)
@@ -43,4 +56,10 @@ class CacheAddress():
             self.blockNumber = int(self.binary[18:], 2)
 
     def __str__(self):
-        return hex(self.binary), CacheAddress.typeArray[self.blockType][0]
+        string = hex(self.addr) + " ("
+        if self.blockType != CacheAddress.SEPARATE_FILE:
+            string += str(self.contiguousBlock) +\
+                      " contiguous blocks in "
+        string += CacheAddress.typeArray[self.blockType][0] +\
+                  " : " + self.fileSelector + ")"
+        return string
