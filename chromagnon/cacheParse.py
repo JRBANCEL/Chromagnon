6f5fe25ae726eba24711ae4914a7a5f3e25062fc
Jean-Rémy Bancel <jrbleboss@gmail.com>
2012-04-22 19:00:57 +0200

    Moving CacheBlock to its own file.

--- chromagnon/cacheParse.py
+++ chromagnon/cacheParse.py
@@ -11,60 +11,13 @@ import os
 import struct
 
 from cacheAddress import CacheAddress
+from cacheBlock import CacheBlock
+from cacheData import CacheData
 from cacheEntry import CacheEntry
 
-class CacheBlock():
-    """
-    Object representing a block of the cache. It can be the index file or any
-    other block type : 256B, 1024B, 4096B, Ranking Block.
-    See /net/disk_cache/disk_format.h for details.
-    """
-
-    INDEX_MAGIC = 0xC103CAC3
-    BLOCK_MAGIC = 0xC104CAC3
-    INDEX = 0
-    BLOCK = 1
-
-    #XXX Filename
-    def __init__(self, filename="../data/Cache"):
-        """
-        Parse the header of a cache file
-        """
-        header = open(filename, 'rB')
-
-        # Read Magic Number
-        magic = struct.unpack('I', header.read(4))[0]
-        if magic == CacheBlock.BLOCK_MAGIC:
-            self.type = CacheBlock.BLOCK
-            header.seek(2, 1)
-            self.version = struct.unpack('h', header.read(2))[0]
-            self.header = struct.unpack('h', header.read(2))[0]
-            self.nextFile = struct.unpack('h', header.read(2))[0]
-            self.blockSize = struct.unpack('I', header.read(4))[0]
-            self.entryCount = struct.unpack('I', header.read(4))[0]
-            self.entryMax = struct.unpack('I', header.read(4))[0]
-            self.empty = []
-            for i in range(4):
-                self.empty.append(struct.unpack('I', header.read(4))[0])
-            self.position = []
-            for i in range(4):
-                self.position.append(struct.unpack('I', header.read(4))[0])
-        elif magic == CacheBlock.INDEX_MAGIC:
-            self.type = CacheBlock.INDEX
-            header.seek(2, 1)
-            self.version = struct.unpack('h', header.read(2))[0]
-            self.entryCount = struct.unpack('I', header.read(4))[0]
-            self.byteCount = struct.unpack('I', header.read(4))[0]
-            self.lastFileCreated = "f_%06x"%struct.unpack('I', header.read(4))[0]
-            header.seek(4*3, 1)
-            self.tableSize = struct.unpack('I', header.read(4))[0]
-        else:
-            header.close()
-            raise Exception("Invalid Chrome Cache File")
-        header.close()
 
 #XXX Filename
-def parse(path="../data/Cache/"):
+def parse(path="/home/jrb/.cache/chromium/Default/Cache/"):
     """
     Reads the whole cache and store the collected data in a table
     """
@@ -85,4 +38,10 @@ def parse(path="../data/Cache/"):
         raw = struct.unpack('I', index.read(4))[0]
         if raw != 0:
             print "------------------------------------------------------------"
-            print CacheEntry(CacheAddress(raw, path=path))
+            print "0x%08x"%key
+            e = CacheEntry(CacheAddress(raw, path=path))
+            print e
+            for i in range(len(e.data)):
+                if e.data[i].type == CacheData.UNKNOWN:
+                    e.data[i].save("/tmp/out/" + hex(e.hash) + "_" + str(i))
+
