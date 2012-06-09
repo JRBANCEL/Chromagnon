323bb416a899024548e4e3c4718bdfa5728deb47
Jean-Rémy Bancel <jrbleboss@gmail.com>
2012-06-09 19:28:17 +0200

    JSON Output.

---
+++ chromagnon/jsonOutput.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+JSON Output Module
+"""
+
+import json
+
+def jsonOutput(queryResult, separator):
+    """
+    Display the data separated in JSON
+    """
+
+    print json.JSONEncoder().encode(queryResult)
