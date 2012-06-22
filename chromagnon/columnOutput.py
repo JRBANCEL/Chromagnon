6f8c10b1226a8bad75d42df9f3748088139ce821
Jean-Rémy Bancel <jrbleboss@gmail.com>
2012-06-22 16:39:58 +0200

    Fixing potential problem in case of non string item.

--- chromagnon/columnOutput.py
+++ chromagnon/columnOutput.py
@@ -11,9 +11,9 @@ def columnOutput(queryResult, separator=' '):
     """
 
     # Finding width of columns
-    size = [max([len(line[i]) for line in queryResult])
+    size = [max([len(str(line[i])) for line in queryResult])
             for i in range(len(queryResult[0]))]
-    # Generating format string
+    # Generating format string (without last separator)
     string = (''.join(["%%-%ds%s"%(x, separator) for x in size]))[:-len(separator)]
     for line in queryResult:
         print string%tuple(line)
